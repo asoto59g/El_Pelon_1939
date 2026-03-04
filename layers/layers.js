dfdb6b6f1a87f5a58279ebf35590c22ddd27fe47
ALEJANDRO SOTO BARQUERO <oasotob@yahoo.com>
2026-03-04 08:13:50 -0600

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -11,81 +11,69 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
             })
         });
-var lyr_pelon1939_1 = new ol.layer.Image({
-        opacity: 1,
-        
-    title: 'pelon1939<br />' ,
-        
-        
-        source: new ol.source.ImageStatic({
-            url: "./layers/pelon1939_1.png",
+
+        var lyr_Fondo_1 = new ol.layer.Tile({
+            'title': 'Fondo',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
             attributions: ' ',
-            projection: 'EPSG:3857',
-            alwaysInRange: true,
-            imageExtent: [-9518130.977848, 1163131.014895, -9500978.644076, 1184883.606755]
-        })
-    });
-var format_Apartos_2 = new ol.format.GeoJSON();
-var features_Apartos_2 = format_Apartos_2.readFeatures(json_Apartos_2, 
+                url: 'file:///C:/Users/AlejandroSotoBarquer/OneDrive - ABC Geomática Agricola SRL/Documentos/ABC_Gis_Activos/01_Clientes/2026/Grandes Haciendas Guanacaste Antaño/El Pelon de la Bajura 1939/Mapa_Hacienda_1939_Pro/fondo/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_apartos_2 = new ol.layer.Tile({
+            'title': 'apartos',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'file:///C:/Users/AlejandroSotoBarquer/OneDrive - ABC Geomática Agricola SRL/Documentos/ABC_Gis_Activos/01_Clientes/2026/Grandes Haciendas Guanacaste Antaño/El Pelon de la Bajura 1939/Mapa_Hacienda_1939_Pro/apartos/{z}/{x}/{y}.png'
+            })
+        });
+var format_Caminos_3 = new ol.format.GeoJSON();
+var features_Caminos_3 = format_Caminos_3.readFeatures(json_Caminos_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Apartos_2 = new ol.source.Vector({
+var jsonSource_Caminos_3 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Apartos_2.addFeatures(features_Apartos_2);
-var lyr_Apartos_2 = new ol.layer.Vector({
+jsonSource_Caminos_3.addFeatures(features_Caminos_3);
+var lyr_Caminos_3 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Apartos_2, 
-                style: style_Apartos_2,
-                popuplayertitle: 'Apartos',
-                interactive: true,
-    title: 'Apartos<br />\
-    <img src="styles/legend/Apartos_2_0.png" /> Baldioceda<br />\
-    <img src="styles/legend/Apartos_2_1.png" /> Bosque<br />\
-    <img src="styles/legend/Apartos_2_2.png" /> Chicharron<br />\
-    <img src="styles/legend/Apartos_2_3.png" /> Desembocadura<br />\
-    <img src="styles/legend/Apartos_2_4.png" /> Intermitente<br />\
-    <img src="styles/legend/Apartos_2_5.png" /> La Isla<br />\
-    <img src="styles/legend/Apartos_2_6.png" /> Panzada<br />\
-    <img src="styles/legend/Apartos_2_7.png" /> Pastizales<br />\
-    <img src="styles/legend/Apartos_2_8.png" /> Pochotada<br />\
-    <img src="styles/legend/Apartos_2_9.png" /> Potrero<br />\
-    <img src="styles/legend/Apartos_2_10.png" /> Secundario<br />\
-    <img src="styles/legend/Apartos_2_11.png" /> Solar<br />\
-    <img src="styles/legend/Apartos_2_12.png" /> Zapal<br />' });
-var format_Rios_3 = new ol.format.GeoJSON();
-var features_Rios_3 = format_Rios_3.readFeatures(json_Rios_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Rios_3 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_Rios_3.addFeatures(features_Rios_3);
-var lyr_Rios_3 = new ol.layer.Vector({
-                declutter: false,
-                source:jsonSource_Rios_3, 
-                style: style_Rios_3,
-                popuplayertitle: 'Rios',
-                interactive: true,
-                title: '<img src="styles/legend/Rios_3.png" /> Rios'
-            });
-var format_Caminos_4 = new ol.format.GeoJSON();
-var features_Caminos_4 = format_Caminos_4.readFeatures(json_Caminos_4, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Caminos_4 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_Caminos_4.addFeatures(features_Caminos_4);
-var lyr_Caminos_4 = new ol.layer.Vector({
-                declutter: false,
-                source:jsonSource_Caminos_4, 
-                style: style_Caminos_4,
+                source:jsonSource_Caminos_3, 
+                style: style_Caminos_3,
                 popuplayertitle: 'Caminos',
                 interactive: true,
-                title: '<img src="styles/legend/Caminos_4.png" /> Caminos'
+                title: '<img src="styles/legend/Caminos_3.png" /> Caminos'
             });
-var group_Pelonvect = new ol.layer.Group({
-                                layers: [lyr_Apartos_2,lyr_Rios_3,lyr_Caminos_4,],
+
+        var lyr_rios_4 = new ol.layer.Tile({
+            'title': 'rios',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'file:///C:/Users/AlejandroSotoBarquer/OneDrive - ABC Geomática Agricola SRL/Documentos/ABC_Gis_Activos/01_Clientes/2026/Grandes Haciendas Guanacaste Antaño/El Pelon de la Bajura 1939/Mapa_Hacienda_1939_Pro/rios/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_Colindantes_5 = new ol.layer.Tile({
+            'title': 'Colindantes',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'file:///C:/Users/AlejandroSotoBarquer/OneDrive - ABC Geomática Agricola SRL/Documentos/ABC_Gis_Activos/01_Clientes/2026/Grandes Haciendas Guanacaste Antaño/El Pelon de la Bajura 1939/Mapa_Hacienda_1939_Pro/Colindantes/{z}/{x}/{y}.png'
+            })
+        });
+var group_Pelon1939 = new ol.layer.Group({
+                                layers: [lyr_Fondo_1,lyr_apartos_2,lyr_Caminos_3,lyr_rios_4,lyr_Colindantes_5,],
                                 fold: 'open',
-                                title: 'Pelon vect'});
+                                title: 'Pelon 1939'});
 var group_Cartografia5k = new ol.layer.Group({
                                 layers: [],
                                 fold: 'close',
@@ -107,17 +95,11 @@ var group_MapaCatastral = new ol.layer.Group({
                                 fold: 'open',
                                 title: 'Mapa Catastral'});
 
-lyr_googlesatellitehybrid_0.setVisible(true);lyr_pelon1939_1.setVisible(true);lyr_Apartos_2.setVisible(true);lyr_Rios_3.setVisible(true);lyr_Caminos_4.setVisible(true);
-var layersList = [lyr_googlesatellitehybrid_0,lyr_pelon1939_1,group_Pelonvect];
-lyr_Apartos_2.set('fieldAliases', {'id': 'id', 'Descripcio': 'Descripcio', 'Hacienda': 'Hacienda', 'AreaHa': 'AreaHa', });
-lyr_Rios_3.set('fieldAliases', {'id': 'id', 'Nombre': 'Nombre', });
-lyr_Caminos_4.set('fieldAliases', {'id': 'id', });
-lyr_Apartos_2.set('fieldImages', {'id': 'TextEdit', 'Descripcio': 'TextEdit', 'Hacienda': 'TextEdit', 'AreaHa': 'TextEdit', });
-lyr_Rios_3.set('fieldImages', {'id': 'TextEdit', 'Nombre': 'TextEdit', });
-lyr_Caminos_4.set('fieldImages', {'id': 'TextEdit', });
-lyr_Apartos_2.set('fieldLabels', {'id': 'no label', 'Descripcio': 'no label', 'Hacienda': 'no label', 'AreaHa': 'no label', });
-lyr_Rios_3.set('fieldLabels', {'id': 'no label', 'Nombre': 'no label', });
-lyr_Caminos_4.set('fieldLabels', {'id': 'no label', });
-lyr_Caminos_4.on('precompose', function(evt) {
+lyr_googlesatellitehybrid_0.setVisible(true);lyr_Fondo_1.setVisible(true);lyr_apartos_2.setVisible(true);lyr_Caminos_3.setVisible(true);lyr_rios_4.setVisible(true);lyr_Colindantes_5.setVisible(true);
+var layersList = [lyr_googlesatellitehybrid_0,group_Pelon1939];
+lyr_Caminos_3.set('fieldAliases', {'id': 'id', });
+lyr_Caminos_3.set('fieldImages', {'id': 'TextEdit', });
+lyr_Caminos_3.set('fieldLabels', {'id': 'no label', });
+lyr_Caminos_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
